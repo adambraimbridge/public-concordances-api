114a5212ac20f977852622435a9195eb6e64eefc
Carlos Roman <carlos.roman@ft.com>
2016-02-23 15:31:24 +0000

    Impl ReadByConceptId.

--- concordances/cypher.go
+++ concordances/cypher.go
@@ -3,6 +3,7 @@ package concordances
 import (
 	"fmt"
 
+	"github.com/Financial-Times/neo-model-utils-go/mapper"
 	log "github.com/Sirupsen/logrus"
 	"github.com/jmcvetta/neoism"
 )
@@ -39,31 +40,27 @@ func (pcw CypherDriver) CheckConnectivity() error {
 	return err
 }
 
-type neoReadIdentifier struct {
-	authority       string
-	identifierValue string
-}
-
 type neoReadStruct struct {
-	uuid  string
-	types []string
+	Uuid       string     `json:"uuid"`
+	Types      []string   `json:"types"`
+	Identifier Identifier `json:"identifier"`
 }
 
 func (pcw CypherDriver) ReadByConceptId(identifiers []string) (concordances Concordances, found bool, err error) {
 	concordances = Concordances{}
-	results := []struct {
-		Rs []neoReadStruct
-	}{}
+	results := []struct{ Rs []neoReadStruct }{}
 	query := &neoism.CypherQuery{
-		Statement: `MATCH (p:Concept) where p.uuid in ["0b79b770-e426-334a-b231-f8f37d9a6678", "8138ca3f-b80d-3ef8-ad59-6a9b6ea5f15e"]
-					OPTIONAL MATCH (p)<-[:IDENTIFIES]-(i:Identifier)
-					RETURN p.uuid as uuid, labels(p) as types, collect(i) as identifiers  `,
-		Parameters: neoism.Props{"identifier": identifiers},
-		Result:     &results,
+		Statement: `
+		MATCH (p:Concept) where p.uuid in {identifiers}
+		MATCH (p)<-[:IDENTIFIES]-(i:Identifier)
+		RETURN collect({uuid:p.uuid, types:labels(p), Identifier:{authority:i.authority, identifierValue:i.value}}) as rs
+		`,
+		Parameters: neoism.Props{"identifiers": identifiers},
+		Result: &results,
 	}
 
 	err = pcw.db.Cypher(query)
-	fmt.Printf("neo:", err)
+	fmt.Printf("neo_err:%s\n", err)
 	if err != nil {
 		log.Errorf("Error looking up uuid %s with query %s from neoism: %+v\n", identifiers, query.Statement, err)
 		return Concordances{}, false, fmt.Errorf("Error accessing Concordance datastore for identifier: %s", identifiers)
@@ -71,21 +68,33 @@ func (pcw CypherDriver) ReadByConceptId(identifiers []string) (concordances Conc
 
 	log.Debugf("CypherResult ReadConcordance for identifier: %s was: %+v", identifiers, results)
 
-	fmt.Printf("RESULTS:", results[0])
-	fmt.Printf("RESULTS1:", len(results))
-	fmt.Printf("RESULTS2:", len(results[0].Rs))
-	if (len(results)) == 0 || len(results[0].Rs) == 0 {
-		fmt.Printf("ARGH")
+	fmt.Printf("RESULTS:%s\n", results)
+	if (len(results)) == 0 {
+		fmt.Printf("ARGH\n")
 		return Concordances{}, false, nil
 	}
 
-	concordances = neoReadStructToConcordances(results[0].Rs)
+	concordances = neoReadStructToConcordances(results[0].Rs, pcw.env)
 	log.Debugf("Returning %v", concordances)
 	return concordances, true, nil
 }
 
-func neoReadStructToConcordances(neo []neoReadStruct) Concordances {
-	fmt.Printf("IN FUNC:", neo)
-	//TODO
-	return Concordances{}
+func (pcw CypherDriver) ReadByAuthority(authority string, ids []string) (concordances Concordances, found bool, err error) {
+	return Concordances{}, false, nil
+}
+
+func neoReadStructToConcordances(neo []neoReadStruct, env string) Concordances {
+	var concordances = Concordances{
+		Concordance: make([]Concordance, len(neo)),
+	}
+	for i, neoCon := range neo {
+		var con = Concordance{}
+		var concept = Concept{}
+		concept.ID = neoCon.Uuid
+		concept.APIURL = mapper.APIURL(neoCon.Uuid, neoCon.Types, env)
+		con.Concept = concept
+		con.Identifier = neoCon.Identifier
+		concordances.Concordance[i] = con
+	}
+	return concordances
 }
--- concordances/cypher_test.go
+++ concordances/cypher_test.go
@@ -1,6 +1,12 @@
 package concordances
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/Financial-Times/base-ft-rw-app-go/baseftrwapp"
+	"github.com/Financial-Times/neo-utils-go/neoutils"
+	"github.com/Financial-Times/organisations-rw-neo4j/organisations"
+	"github.com/Financial-Times/people-rw-neo4j/people"
 	"github.com/jmcvetta/neoism"
 	"github.com/stretchr/testify/assert"
 	"os"
@@ -12,13 +18,25 @@ func TestNeoReadStructToConcordancesMandatoryFields(t *testing.T) {
 
 	assert := assert.New(t)
 	db := getDatabaseConnectionAndCheckClean(t, assert)
-	//	batchRunner := neoutils.NewBatchCypherRunner(neoutils.StringerDb{db}, 1)
+	batchRunner := neoutils.NewBatchCypherRunner(neoutils.StringerDb{db}, 1)
+
+	peopleRW, organisationRW := getServices(t, assert, db, &batchRunner)
+	writeJsonToService(peopleRW, "./fixtures/Person-Dan_Murphy-868c3c17-611c-4943-9499-600ccded71f3.json", assert)
+	writeJsonToService(organisationRW, "./fixtures/Organisation-Child-f21a5cc0-d326-4e62-b84a-d840c2209fee.json", assert)
 
 	undertest := NewCypherDriver(db, "prod")
-	cs, found, err := undertest.ReadByConceptId([]string{"3e844449-b27f-40d4-b696-2ce9b6137133"})
+	cs, found, err := undertest.ReadByConceptId([]string{"868c3c17-611c-4943-9499-600ccded71f3"})
 	assert.NoError(err)
 	assert.True(found)
 	assert.NotEmpty(cs.Concordance)
+	fmt.Printf("RESULTS:%s\n", cs)
+}
+func getServices(t *testing.T, assert *assert.Assertions, db *neoism.Database, batchRunner *neoutils.CypherRunner) (baseftrwapp.Service, baseftrwapp.Service) {
+	peopleRW := people.NewCypherPeopleService(*batchRunner, db)
+	assert.NoError(peopleRW.Initialise())
+	organisationRW := organisations.NewCypherOrganisationService(*batchRunner, db)
+	assert.NoError(organisationRW.Initialise())
+	return peopleRW, organisationRW
 }
 
 func getDatabaseConnectionAndCheckClean(t *testing.T, assert *assert.Assertions) *neoism.Database {
@@ -40,5 +58,30 @@ func getDatabaseConnection(t *testing.T, assert *assert.Assertions) *neoism.Data
 }
 
 func cleanDB(db *neoism.Database, t *testing.T, assert *assert.Assertions) {
+	uuids := []string{
+		"f21a5cc0-d326-4e62-b84a-d840c2209fee",
+		"868c3c17-611c-4943-9499-600ccded71f3",
+	}
 
+	qs := make([]*neoism.CypherQuery, len(uuids))
+	for i, uuid := range uuids {
+		qs[i] = &neoism.CypherQuery{
+			Statement: fmt.Sprintf("MATCH (a:Thing {uuid: '%s'}) DETACH DELETE a", uuid)}
+	}
+	err := db.CypherBatch(qs)
+	assert.NoError(err)
+}
+
+func writeJsonToService(service baseftrwapp.Service, pathToJsonFile string, assert *assert.Assertions) {
+	f, err := os.Open(pathToJsonFile)
+	assert.NoError(err)
+	dec := json.NewDecoder(f)
+	inst, _, errr := service.DecodeJSON(dec)
+	assert.NoError(errr)
+	errrr := service.Write(inst)
+	assert.NoError(errrr)
+}
+func deleteAllViaService(assert *assert.Assertions, peopleRW baseftrwapp.Service, organisationRW baseftrwapp.Service) {
+	peopleRW.Delete("868c3c17-611c-4943-9499-600ccded71f3")
+	organisationRW.Delete("f21a5cc0-d326-4e62-b84a-d840c2209fee")
 }
