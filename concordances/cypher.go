d787840af0aae0b126e2301d65db0beb128df918
Emil Dimitrov <emil.dimitrov@ft.com>
2019-03-25 10:20:40 +0200

    Update cypher queries

--- concordances/cypher.go
+++ concordances/cypher.go
@@ -42,11 +42,18 @@ func (pcw CypherDriver) ReadByConceptID(identifiers []string) (concordances Conc
 		MATCH (canonical)<-[:EQUIVALENT_TO]-(leafNode:Thing)
 		RETURN DISTINCT canonical.prefUUID AS canonicalUUID, labels(canonical) AS types, leafNode.authority as authority, leafNode.authorityValue as authorityValue
 		UNION ALL
+
 		MATCH (p:Thing)
 		WHERE p.uuid in {identifiers}
 		MATCH (p)-[:EQUIVALENT_TO]->(canonical:Concept)
 		WHERE exists(canonical.leiCode)
 		RETURN DISTINCT canonical.prefUUID AS canonicalUUID, labels(canonical) AS types, 'LEI' as authority, canonical.leiCode as authorityValue
+		UNION ALL
+		MATCH (p:Thing)
+		WHERE p.uuid in {identifiers}
+		MATCH (p)-[:EQUIVALENT_TO]->(canonical:Concept)
+		WHERE exists(canonical.iso13661)
+		RETURN DISTINCT canonical.prefUUID AS canonicalUUID, labels(canonical) AS types, 'ISO-1366-1' as authority, canonical.iso13661 as authorityValue
 		UNION ALL
 		MATCH (p:Thing)
 		WHERE p.uuid in {identifiers}
@@ -114,17 +121,21 @@ func (pcw CypherDriver) ReadByAuthority(authority string, identifierValues []str
 			},
 			Result: &results,
 		}
-	} else if authorityProperty == "IsoCode" {
+	} else if authorityProperty == "ISO-1366-1" {
 		//hacky way to retrieve prefUUID of the canonical
 		// via matching the managedlocation source by isoCode
 		query = &neoism.CypherQuery{
 			Statement: `
 		MATCH (p:Concept)
-		WHERE exists(p.isoCode)
-		WHERE p.isoCode IN {authorityValue}
+		WHERE exists(p.iso13661)
+		AND p.iso13661 IN {authorityValue}
 		MATCH (p)-[:EQUIVALENT_TO]->(canonical:Concept)
-		RETURN DISTINCT canonical.prefUUID AS canonicalUUID, labels(canonical) AS types, p.uuid as UUID, 'ISO-3166-1' as authority, p.isoCode as authorityValue
+		RETURN DISTINCT canonical.prefUUID AS canonicalUUID, labels(canonical) AS types, p.uuid as UUID, 'ISO-1366-1' as authority, p.iso13661 as authorityValue
 			`,
+			Parameters: neoism.Props{
+				"authorityValue": identifierValues,
+			},
+			Result: &results,
 		}
 	} else {
 		query = &neoism.CypherQuery{
@@ -205,7 +216,7 @@ var authorityMap = map[string]string{
 	"LEI":             "http://api.ft.com/system/LEI",
 	"Smartlogic":      "http://api.ft.com/system/SMARTLOGIC",
 	"ManagedLocation": "http://api.ft.com/system/MANAGEDLOCATION",
-	"IsoCode":         "http://api.ft.com/system/ISO-3166-1",
+	"ISO-1366-1":      "http://api.ft.com/system/ISO-1366-1",
 	"Geonames":        "http://api.ft.com/system/GEONAMES",
 	"Wikidata":        "http://api.ft.com/system/WIKIDATA",
 	"DBPedia":         "http://api.ft.com/system/DBPEDIA",
